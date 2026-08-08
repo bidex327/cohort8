0fc0170a5cb7b2de45f4bf83940ad636d39fc33e
bidex327 <adebayobidemi303@gmail.com>
2026-08-08 02:37:19 +0100

    Updated object.js

---
+++ object.js
@@ -0,0 +1,41 @@
+//adding and updating Props to an object
+const students = {
+  name: "Abidemi",
+  age: 78,
+  regNo: "c8/0229",
+  course: "javascript",
+  year: 2026,
+  friends: ["tunde", "shola", "femi", "kunle"],
+};
+
+console.log(students);
+students.gender = "male";
+console.log(students);
+
+students.course = "node_js";
+console.log(students);
+delete students.course;
+console.log(students);
+
+const phone = {
+  brand: "Samsung",
+  price: 50000,
+  model: "Galaxys9",
+};
+phone.color = "red";
+console.log(phone);
+
+
+let employee = {
+    name: "tunde",
+    role: "manager",
+    salary: 7666 ,
+    city: "lagos",
+};
+
+let hisName = employee.name
+let hisRole = employee.role
+let hisSalary = employee.salary
+let hisCity = employee.city
+
+const {name, role, salary, city}= employee;
